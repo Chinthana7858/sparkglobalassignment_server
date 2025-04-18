015d46ad77ad5e15f5c7139d8d8643754156cb8d
Chinthana Prabhashitha <prabhashitharac.20@uom.lk>
2025-04-18 21:31:14 +0530

    route_issue_fix

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,7 +12,7 @@ const {
 router.post('/', upload.single('image'), createProduct);
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
-router.put('/:id', upload.single('image'), updateProduct);
-router.delete('/:id', deleteProduct);
+router.put('/update/:id', upload.single('image'), updateProduct);
+router.delete('/delete/:id', deleteProduct);
 
 module.exports = router;
